fb22ef49d85c27e67e627b61f297c065b411adb1
Crowdbotics <team@crowdbotics.com>
2024-05-29 01:50:18 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cryptoplay-48151",
+  url: "https://cryptoplay-48151.botics.co",
+  api: "https://cryptoplay-48151.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
